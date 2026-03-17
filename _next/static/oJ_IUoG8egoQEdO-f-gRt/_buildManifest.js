fe76aa10bac727e77c4b2a798aaef01e6992b31e
silver <silver@silvers-MacBook-Pro.local>
2026-03-17 22:58:56 +0800

    Updates

--- _next/static/oJ_IUoG8egoQEdO-f-gRt/_buildManifest.js
+++ _next/static/oJ_IUoG8egoQEdO-f-gRt/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/aspen-investor//_next/:path+",
-        "destination": "/aspen-investor/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
